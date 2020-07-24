91729850355673d6859b766cbfc8df9f0c85081a
gttkeith <gttkeith@gmail.com>
2020-07-24 12:14:46 +0800

    csv dumping query functionality

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Create an app on your developer account (for either DEMO or LIVE) using the foll
 * Grant Type: Code
 * Access control: ☑ Allow this app to be enabled for trading
 
-An example main.py has been provided in the repo, where app details are stored in a params.json file.
+An example main.py has been provided in the repo, where app details are stored in a params.json file. Queries can be made through the console, and results are dumped into `out.csv` in the same directory.
 
 pysaxo negotiates the authentication procedure using the complete OAuth2 authorisation flow. Sessions are proactively refreshed to prevent unexpected logouts.
 
@@ -33,6 +33,9 @@ access.get('port/v1/users/me')
 
 # retrieve a list of FX spot instruments containing SGD
 access.get('ref/v1/instruments', KeyWords='SGD', AssetTypes='FxSpot')
+
+# retrieve the historical 1H bid/ask prices of asset UIC 45: USDSGD
+access.get('chart/v1/charts', Uic=45, AssetType='FxSpot', Horizon=60)
 ```
 
 Query parameters are case-sensitive.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import pprint
 import traceback
@@ -11,18 +12,31 @@ AUTH_ENDPOINT = params.get('auth_endpoint')
 TOKEN_ENDPOINT = params.get('token_endpoint')
 SECRET = params.get('secret')
 
-access = Session(APP_KEY, AUTH_ENDPOINT, TOKEN_ENDPOINT, SECRET)
+def csv_export(jsondict):
+    w = csv.writer(open('out.csv','w'), dialect='excel')
+    data = jsondict.get('Data')
+    if data:
+        schema = data[0].keys()
+        w.writerow(schema)
+        for row in data:
+            w.writerow([row.get(k) for k in schema])
+    else:
+        for k,v in jsondict.items():
+            w.writerow([k,v])
 
-print("[Authorised, use access.get/post/put/delete(uri) for API access]")
-exit=False
+access = Session(APP_KEY, AUTH_ENDPOINT, TOKEN_ENDPOINT, SECRET)
+print("[Authorised, use get/post/put/delete(uri) for API access]")
+
 pp = pprint.PrettyPrinter(indent=2)
+exit=False
 while exit is False:
     cmd=input('> ')
     try:
         if cmd is 'exit':
             exit = True
         else:
-            out=eval(cmd)
+            out=eval('access.'+cmd)
             pp.pprint(out)
+            csv_export(out)
     except:
         print("\n** EXCEPTION **\n",traceback.format_exc(),"\n")
